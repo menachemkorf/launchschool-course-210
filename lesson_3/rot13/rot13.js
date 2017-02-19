3e6da97510a0d7c8312218304852ec82d3939d87
Menachem Korf <menachemkorf@gmail.com>
2017-02-19 11:15:15 +1100

    revised rot13 exercise

--- lesson_3/rot13/rot13.js
+++ lesson_3/rot13/rot13.js
@@ -1,26 +1,37 @@
+var baseCharCodes = {
+  'a': 'a'.charCodeAt(0),
+  'z': 'z'.charCodeAt(0),
+  'A': 'A'.charCodeAt(0),
+  'Z': 'Z'.charCodeAt(0),
+}
+
+var halfLength = 12;
+var rotateCount = 13;
+
+function isFirstAlphabetHalf(charCode) {
+  return (charCode >= baseCharCodes['A'] &&
+           charCode < (baseCharCodes['A'] + halfLength)) ||
+          (charCode >= baseCharCodes['a'] &&
+           charCode < (baseCharCodes['a'] + halfLength));
+}
+
+function isLastAlphabetHalf(charCode) {
+  return (charCode <= baseCharCodes['Z'] &&
+          charCode > (baseCharCodes['Z'] - halfLength)) ||
+         (charCode <= baseCharCodes['z'] &&
+          charCode > (baseCharCodes['z'] - halfLength));
+}
+
 function rot13(string) {
   var newString = '';
   var asciiNumeric;
 
-  var baseCharCodes = {
-    'a': 'a'.charCodeAt(0),
-    'z': 'z'.charCodeAt(0),
-    'A': 'A'.charCodeAt(0),
-    'Z': 'Z'.charCodeAt(0),
-  }
-
   for (var i = 0; i < string.length; i++) {
     asciiNumeric = string.charCodeAt(i);
-    if ((asciiNumeric >= baseCharCodes['A'] &&
-         asciiNumeric < (baseCharCodes['A'] + 12)) ||
-        (asciiNumeric >= baseCharCodes['a'] &&
-         asciiNumeric < (baseCharCodes['a'] + 12))) {
-      newString += String.fromCharCode(asciiNumeric + 13);
-    } else if ((asciiNumeric <= baseCharCodes['Z'] &&
-                asciiNumeric > (baseCharCodes['Z'] - 12)) ||
-               (asciiNumeric <= baseCharCodes['z'] &&
-                asciiNumeric > (baseCharCodes['z'] - 12))) {
-      newString += String.fromCharCode(asciiNumeric - 13);
+    if (isFirstAlphabetHalf(asciiNumeric)) {
+      newString += String.fromCharCode(asciiNumeric + rotateCount);
+    } else if (isLastAlphabetHalf(asciiNumeric)) {
+      newString += String.fromCharCode(asciiNumeric - rotateCount);
     } else {
       newString += string[i];
     }
